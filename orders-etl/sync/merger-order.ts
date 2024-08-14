f55ecd99b2ac37e6ada535a556da54248b9701ad
iliyan-velichkov <velichkov.iliyan@gmail.com>
2024-08-14 14:35:42 +0300

    remove EDM and use database API instead

--- orders-etl/sync/merger-order.ts
+++ orders-etl/sync/merger-order.ts
@@ -1,21 +1,37 @@
 import { oc_orderEntity } from "orders-etl/dao/oc_orderRepository";
-import { OrderRepository } from "orders-etl/gen/orders-etl/dao/entities/OrderRepository";
+import { database } from "sdk/db";
 
 export function onMessage(message: any) {
     const openCartOrder: oc_orderEntity = message.getBody();
 
-    console.log("About to upsert Open cart order " + JSON.stringify(openCartOrder));
+    console.log(`About to upsert Open cart order [${openCartOrder.order_id}]...`);
+    upsertOrder(openCartOrder);
+    console.log(`Upserted Open cart order [${openCartOrder.order_id}]`);
 
+    return message;
+}
+
+const MERGE_SQL = `
+    MERGE INTO ORDERS
+        (ID, TOTAL, DATEADDED) 
+    KEY(ID)
+    VALUES (?, ?, ?)
+`;
+
+function upsertOrder(openCartOrder: oc_orderEntity) {
     // todo get rates from API
     const totalEuro = openCartOrder.total;
 
-    const repository = new OrderRepository();
-    repository.upsert({
-        Id: openCartOrder.order_id,
-        Total: totalEuro,
-        DateAdded: openCartOrder.date_added
-    });
-    console.log("Upserted entity.");
+    const connection = database.getConnection();
+    const statement = connection.prepareStatement(MERGE_SQL);
+    try {
+        statement.setLong(1, openCartOrder.order_id);
+        statement.setDouble(2, totalEuro);
+        statement.setTimestamp(3, openCartOrder.date_added);
+        statement.executeUpdate();
+    } finally {
+        statement.close();
+        connection.close();
+    }
 
-    return message;
 }
